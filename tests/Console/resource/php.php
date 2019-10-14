22fc7a76e04be8bf6bc6b96ae26c353ddadea6f3
Christoph Kappestein <christoph.kappestein@gmail.com>
2019-10-14 20:27:01 +0200

    fix test cases

--- tests/Console/resource/php.php
+++ tests/Console/resource/php.php
@@ -17,10 +17,16 @@ class Meta extends \ArrayObject
      * @Format("date-time")
      */
     protected $createDate;
+    /**
+     * @param \DateTime $createDate
+     */
     public function setCreateDate(?\DateTime $createDate)
     {
         $this->createDate = $createDate;
     }
+    /**
+     * @return \DateTime
+     */
     public function getCreateDate() : ?\DateTime
     {
         return $this->createDate;
@@ -46,18 +52,30 @@ class Web extends \ArrayObject
      * @Type("string")
      */
     protected $url;
+    /**
+     * @param string $name
+     */
     public function setName(?string $name)
     {
         $this->name = $name;
     }
+    /**
+     * @return string
+     */
     public function getName() : ?string
     {
         return $this->name;
     }
+    /**
+     * @param string $url
+     */
     public function setUrl(?string $url)
     {
         $this->url = $url;
     }
+    /**
+     * @return string
+     */
     public function getUrl() : ?string
     {
         return $this->url;
@@ -81,18 +99,30 @@ class Location extends \ArrayObject
      * @Type("number")
      */
     protected $long;
+    /**
+     * @param float $lat
+     */
     public function setLat(?float $lat)
     {
         $this->lat = $lat;
     }
+    /**
+     * @return float
+     */
     public function getLat() : ?float
     {
         return $this->lat;
     }
+    /**
+     * @param float $long
+     */
     public function setLong(?float $long)
     {
         $this->long = $long;
     }
+    /**
+     * @return float
+     */
     public function getLong() : ?float
     {
         return $this->long;
@@ -137,42 +167,72 @@ class Author
      * @Ref("PSX\Generation\Location")
      */
     protected $origin;
+    /**
+     * @param string $title
+     */
     public function setTitle(?string $title)
     {
         $this->title = $title;
     }
+    /**
+     * @return string
+     */
     public function getTitle() : ?string
     {
         return $this->title;
     }
+    /**
+     * @param string $email
+     */
     public function setEmail(?string $email)
     {
         $this->email = $email;
     }
+    /**
+     * @return string
+     */
     public function getEmail() : ?string
     {
         return $this->email;
     }
+    /**
+     * @param array<string> $categories
+     */
     public function setCategories(?array $categories)
     {
         $this->categories = $categories;
     }
+    /**
+     * @return array<string>
+     */
     public function getCategories() : ?array
     {
         return $this->categories;
     }
+    /**
+     * @param array<Location> $locations
+     */
     public function setLocations(?array $locations)
     {
         $this->locations = $locations;
     }
+    /**
+     * @return array<Location>
+     */
     public function getLocations() : ?array
     {
         return $this->locations;
     }
+    /**
+     * @param Location $origin
+     */
     public function setOrigin(?Location $origin)
     {
         $this->origin = $origin;
     }
+    /**
+     * @return Location
+     */
     public function getOrigin() : ?Location
     {
         return $this->origin;
@@ -308,154 +368,268 @@ class News
      * @Type("string")
      */
     protected $captcha;
+    /**
+     * @param Config $config
+     */
     public function setConfig(?Config $config)
     {
         $this->config = $config;
     }
+    /**
+     * @return Config
+     */
     public function getConfig() : ?Config
     {
         return $this->config;
     }
+    /**
+     * @param array<string> $tags
+     */
     public function setTags(?array $tags)
     {
         $this->tags = $tags;
     }
+    /**
+     * @return array<string>
+     */
     public function getTags() : ?array
     {
         return $this->tags;
     }
+    /**
+     * @param array<Author> $receiver
+     */
     public function setReceiver(?array $receiver)
     {
         $this->receiver = $receiver;
     }
+    /**
+     * @return array<Author>
+     */
     public function getReceiver() : ?array
     {
         return $this->receiver;
     }
+    /**
+     * @param array<Location|Web> $resources
+     */
     public function setResources(?array $resources)
     {
         $this->resources = $resources;
     }
+    /**
+     * @return array<Location|Web>
+     */
     public function getResources() : ?array
     {
         return $this->resources;
     }
+    /**
+     * @param string $profileImage
+     */
     public function setProfileImage(?string $profileImage)
     {
         $this->profileImage = $profileImage;
     }
+    /**
+     * @return string
+     */
     public function getProfileImage() : ?string
     {
         return $this->profileImage;
     }
+    /**
+     * @param bool $read
+     */
     public function setRead(?bool $read)
     {
         $this->read = $read;
     }
+    /**
+     * @return bool
+     */
     public function getRead() : ?bool
     {
         return $this->read;
     }
+    /**
+     * @param Author|Web $source
+     */
     public function setSource($source)
     {
         $this->source = $source;
     }
+    /**
+     * @return Author|Web
+     */
     public function getSource()
     {
         return $this->source;
     }
+    /**
+     * @param Author $author
+     */
     public function setAuthor(?Author $author)
     {
         $this->author = $author;
     }
+    /**
+     * @return Author
+     */
     public function getAuthor() : ?Author
     {
         return $this->author;
     }
+    /**
+     * @param Meta $meta
+     */
     public function setMeta(?Meta $meta)
     {
         $this->meta = $meta;
     }
+    /**
+     * @return Meta
+     */
     public function getMeta() : ?Meta
     {
         return $this->meta;
     }
+    /**
+     * @param \DateTime $sendDate
+     */
     public function setSendDate(?\DateTime $sendDate)
     {
         $this->sendDate = $sendDate;
     }
+    /**
+     * @return \DateTime
+     */
     public function getSendDate() : ?\DateTime
     {
         return $this->sendDate;
     }
+    /**
+     * @param \DateTime $readDate
+     */
     public function setReadDate(?\DateTime $readDate)
     {
         $this->readDate = $readDate;
     }
+    /**
+     * @return \DateTime
+     */
     public function getReadDate() : ?\DateTime
     {
         return $this->readDate;
     }
+    /**
+     * @param \DateInterval $expires
+     */
     public function setExpires(?\DateInterval $expires)
     {
         $this->expires = $expires;
     }
+    /**
+     * @return \DateInterval
+     */
     public function getExpires() : ?\DateInterval
     {
         return $this->expires;
     }
+    /**
+     * @param float $price
+     */
     public function setPrice(?float $price)
     {
         $this->price = $price;
     }
+    /**
+     * @return float
+     */
     public function getPrice() : ?float
     {
         return $this->price;
     }
+    /**
+     * @param int $rating
+     */
     public function setRating(?int $rating)
     {
         $this->rating = $rating;
     }
+    /**
+     * @return int
+     */
     public function getRating() : ?int
     {
         return $this->rating;
     }
+    /**
+     * @param string $content
+     */
     public function setContent(?string $content)
     {
         $this->content = $content;
     }
+    /**
+     * @return string
+     */
     public function getContent() : ?string
     {
         return $this->content;
     }
+    /**
+     * @param string $question
+     */
     public function setQuestion(?string $question)
     {
         $this->question = $question;
     }
+    /**
+     * @return string
+     */
     public function getQuestion() : ?string
     {
         return $this->question;
     }
+    /**
+     * @param \DateTime $coffeeTime
+     */
     public function setCoffeeTime(?\DateTime $coffeeTime)
     {
         $this->coffeeTime = $coffeeTime;
     }
+    /**
+     * @return \DateTime
+     */
     public function getCoffeeTime() : ?\DateTime
     {
         return $this->coffeeTime;
     }
+    /**
+     * @param string $profileUri
+     */
     public function setProfileUri(?string $profileUri)
     {
         $this->profileUri = $profileUri;
     }
+    /**
+     * @return string
+     */
     public function getProfileUri() : ?string
     {
         return $this->profileUri;
     }
+    /**
+     * @param string $captcha
+     */
     public function setCaptcha(?string $captcha)
     {
         $this->captcha = $captcha;
     }
+    /**
+     * @return string
+     */
     public function getCaptcha() : ?string
     {
         return $this->captcha;
--- tests/Generator/resource/php_recursive.php
+++ tests/Generator/resource/php_recursive.php
@@ -208,258 +208,450 @@ class Json_schema
      * @Ref("PSX\Generation\Json_schema")
      */
     protected $not;
+    /**
+     * @param string $id
+     */
     public function setId(?string $id)
     {
         $this->id = $id;
     }
+    /**
+     * @return string
+     */
     public function getId() : ?string
     {
         return $this->id;
     }
+    /**
+     * @param string $_schema
+     */
     public function set_schema(?string $_schema)
     {
         $this->_schema = $_schema;
     }
+    /**
+     * @return string
+     */
     public function get_schema() : ?string
     {
         return $this->_schema;
     }
+    /**
+     * @param string $title
+     */
     public function setTitle(?string $title)
     {
         $this->title = $title;
     }
+    /**
+     * @return string
+     */
     public function getTitle() : ?string
     {
         return $this->title;
     }
+    /**
+     * @param string $description
+     */
     public function setDescription(?string $description)
     {
         $this->description = $description;
     }
+    /**
+     * @return string
+     */
     public function getDescription() : ?string
     {
         return $this->description;
     }
+    /**
+     * @param  $default
+     */
     public function setDefault($default)
     {
         $this->default = $default;
     }
+    /**
+     * @return 
+     */
     public function getDefault()
     {
         return $this->default;
     }
+    /**
+     * @param float $multipleOf
+     */
     public function setMultipleOf(?float $multipleOf)
     {
         $this->multipleOf = $multipleOf;
     }
+    /**
+     * @return float
+     */
     public function getMultipleOf() : ?float
     {
         return $this->multipleOf;
     }
+    /**
+     * @param float $maximum
+     */
     public function setMaximum(?float $maximum)
     {
         $this->maximum = $maximum;
     }
+    /**
+     * @return float
+     */
     public function getMaximum() : ?float
     {
         return $this->maximum;
     }
+    /**
+     * @param bool $exclusiveMaximum
+     */
     public function setExclusiveMaximum(?bool $exclusiveMaximum)
     {
         $this->exclusiveMaximum = $exclusiveMaximum;
     }
+    /**
+     * @return bool
+     */
     public function getExclusiveMaximum() : ?bool
     {
         return $this->exclusiveMaximum;
     }
+    /**
+     * @param float $minimum
+     */
     public function setMinimum(?float $minimum)
     {
         $this->minimum = $minimum;
     }
+    /**
+     * @return float
+     */
     public function getMinimum() : ?float
     {
         return $this->minimum;
     }
+    /**
+     * @param bool $exclusiveMinimum
+     */
     public function setExclusiveMinimum(?bool $exclusiveMinimum)
     {
         $this->exclusiveMinimum = $exclusiveMinimum;
     }
+    /**
+     * @return bool
+     */
     public function getExclusiveMinimum() : ?bool
     {
         return $this->exclusiveMinimum;
     }
+    /**
+     * @param int $maxLength
+     */
     public function setMaxLength(?int $maxLength)
     {
         $this->maxLength = $maxLength;
     }
+    /**
+     * @return int
+     */
     public function getMaxLength() : ?int
     {
         return $this->maxLength;
     }
+    /**
+     * @param int& $minLength
+     */
     public function setMinLength($minLength)
     {
         $this->minLength = $minLength;
     }
+    /**
+     * @return int&
+     */
     public function getMinLength()
     {
         return $this->minLength;
     }
+    /**
+     * @param string $pattern
+     */
     public function setPattern(?string $pattern)
     {
         $this->pattern = $pattern;
     }
+    /**
+     * @return string
+     */
     public function getPattern() : ?string
     {
         return $this->pattern;
     }
+    /**
+     * @param  $additionalItems
+     */
     public function setAdditionalItems($additionalItems)
     {
         $this->additionalItems = $additionalItems;
     }
+    /**
+     * @return 
+     */
     public function getAdditionalItems()
     {
         return $this->additionalItems;
     }
+    /**
+     * @param  $items
+     */
     public function setItems($items)
     {
         $this->items = $items;
     }
+    /**
+     * @return 
+     */
     public function getItems()
     {
         return $this->items;
     }
+    /**
+     * @param int $maxItems
+     */
     public function setMaxItems(?int $maxItems)
     {
         $this->maxItems = $maxItems;
     }
+    /**
+     * @return int
+     */
     public function getMaxItems() : ?int
     {
         return $this->maxItems;
     }
+    /**
+     * @param int& $minItems
+     */
     public function setMinItems($minItems)
     {
         $this->minItems = $minItems;
     }
+    /**
+     * @return int&
+     */
     public function getMinItems()
     {
         return $this->minItems;
     }
+    /**
+     * @param bool $uniqueItems
+     */
     public function setUniqueItems(?bool $uniqueItems)
     {
         $this->uniqueItems = $uniqueItems;
     }
+    /**
+     * @return bool
+     */
     public function getUniqueItems() : ?bool
     {
         return $this->uniqueItems;
     }
+    /**
+     * @param int $maxProperties
+     */
     public function setMaxProperties(?int $maxProperties)
     {
         $this->maxProperties = $maxProperties;
     }
+    /**
+     * @return int
+     */
     public function getMaxProperties() : ?int
     {
         return $this->maxProperties;
     }
+    /**
+     * @param int& $minProperties
+     */
     public function setMinProperties($minProperties)
     {
         $this->minProperties = $minProperties;
     }
+    /**
+     * @return int&
+     */
     public function getMinProperties()
     {
         return $this->minProperties;
     }
+    /**
+     * @param array<string> $required
+     */
     public function setRequired(?array $required)
     {
         $this->required = $required;
     }
+    /**
+     * @return array<string>
+     */
     public function getRequired() : ?array
     {
         return $this->required;
     }
+    /**
+     * @param  $additionalProperties
+     */
     public function setAdditionalProperties($additionalProperties)
     {
         $this->additionalProperties = $additionalProperties;
     }
+    /**
+     * @return 
+     */
     public function getAdditionalProperties()
     {
         return $this->additionalProperties;
     }
+    /**
+     * @param ObjectId $definitions
+     */
     public function setDefinitions(?ObjectId $definitions)
     {
         $this->definitions = $definitions;
     }
+    /**
+     * @return ObjectId
+     */
     public function getDefinitions() : ?ObjectId
     {
         return $this->definitions;
     }
+    /**
+     * @param ObjectId $properties
+     */
     public function setProperties(?ObjectId $properties)
     {
         $this->properties = $properties;
     }
+    /**
+     * @return ObjectId
+     */
     public function getProperties() : ?ObjectId
     {
         return $this->properties;
     }
+    /**
+     * @param ObjectId $patternProperties
+     */
     public function setPatternProperties(?ObjectId $patternProperties)
     {
         $this->patternProperties = $patternProperties;
     }
+    /**
+     * @return ObjectId
+     */
     public function getPatternProperties() : ?ObjectId
     {
         return $this->patternProperties;
     }
+    /**
+     * @param ObjectId $dependencies
+     */
     public function setDependencies(?ObjectId $dependencies)
     {
         $this->dependencies = $dependencies;
     }
+    /**
+     * @return ObjectId
+     */
     public function getDependencies() : ?ObjectId
     {
         return $this->dependencies;
     }
+    /**
+     * @param array $enum
+     */
     public function setEnum(?array $enum)
     {
         $this->enum = $enum;
     }
+    /**
+     * @return array
+     */
     public function getEnum() : ?array
     {
         return $this->enum;
     }
+    /**
+     * @param  $type
+     */
     public function setType($type)
     {
         $this->type = $type;
     }
+    /**
+     * @return 
+     */
     public function getType()
     {
         return $this->type;
     }
+    /**
+     * @param array<Json_schema> $allOf
+     */
     public function setAllOf(?array $allOf)
     {
         $this->allOf = $allOf;
     }
+    /**
+     * @return array<Json_schema>
+     */
     public function getAllOf() : ?array
     {
         return $this->allOf;
     }
+    /**
+     * @param array<Json_schema> $anyOf
+     */
     public function setAnyOf(?array $anyOf)
     {
         $this->anyOf = $anyOf;
     }
+    /**
+     * @return array<Json_schema>
+     */
     public function getAnyOf() : ?array
     {
         return $this->anyOf;
     }
+    /**
+     * @param array<Json_schema> $oneOf
+     */
     public function setOneOf(?array $oneOf)
     {
         $this->oneOf = $oneOf;
     }
+    /**
+     * @return array<Json_schema>
+     */
     public function getOneOf() : ?array
     {
         return $this->oneOf;
     }
+    /**
+     * @param Json_schema $not
+     */
     public function setNot(?Json_schema $not)
     {
         $this->not = $not;
     }
+    /**
+     * @return Json_schema
+     */
     public function getNot() : ?Json_schema
     {
         return $this->not;
