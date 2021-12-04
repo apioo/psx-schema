2b2786db35839b901f7b6542b06257bf222ddc26
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-12-04 23:02:02 +0100

    update php doc

--- tests/Document/DocumentTest.php
+++ tests/Document/DocumentTest.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * PSX is a open source PHP framework to develop RESTful APIs.
- * For the current version and informations visit <http://phpsx.org>
+ * PSX is an open source PHP framework to develop RESTful APIs.
+ * For the current version and information visit <https://phpsx.org>
  *
- * Copyright 2010-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
+ * Copyright 2010-2022 Christoph Kappestein <christoph.kappestein@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
