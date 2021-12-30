82f1fd72a01ae5810902e4eebd3b34abe2168115
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-12-30 21:55:48 +0100

    fix test case

--- tests/BinTest.php
+++ tests/BinTest.php
@@ -56,6 +56,7 @@ Options:
   -v|vv|vvv, --verbose  Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug
 
 Available commands:
+  completion    Dump the shell completion script
   help          Display help for a command
   list          List commands
  schema
